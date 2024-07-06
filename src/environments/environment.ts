99687499f918dadf68f1e3872ccbff07f96a48bf
isaiasfontes <51790878+isaiasfontes@users.noreply.github.com>
2024-07-06 08:46:46 -0300

    Ajuste URL NGROK

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    url_voice_calculator :  '', // Inicialmente vazio
+    url_voice_calculator :  'https://69c6-181-233-69-14.ngrok-free.app', // Inicialmente vazio
     projectId: '',
     orgId: ''
 };
